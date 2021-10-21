588f3f1847e2f720277d513872828fd3a9521cc5
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-21 14:28:29 +0200

    optimized the splitting into chunks process

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import { nextTick } from 'process';
 
 const PATH = './data/StockEtablissement_utf8.csv';
 
@@ -8,19 +7,24 @@ function FilterHeaders(header) {
     return [0];
 }
 
-function SaveCSV(lines, filter, savePath) {
-    let data = lines.join("\n");
-    //fs.writeFileSync(savePath, data);
-    console.log(savePath);
+function SaveCSV(buffer, size, filter, savePath) {
+    fs.writeFileSync(savePath, buffer.slice(0, size));
+    console.log(savePath + " " + size);
+}
+
+function FindLastCharacter(character, buffer, bufferSize) {
+    for (let i = bufferSize - 1; i >= 0; --i) {
+        if (String.fromCharCode(buffer[i]) === character) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 function SplitCSV() {
-    let lines = [];
-    const chunkSize = 1024 * 1024;
+    const chunkSize = 1024 * 1024 * 35;
     const chunkBuffer = Buffer.alloc(chunkSize);
 
-    let currentLine = null;
-    let totalLineCount = 0;
     let bytesRead = 0;
     let offset = 0;
     let csvId = 0;
@@ -29,66 +33,26 @@ function SplitCSV() {
     const fp = fs.openSync(PATH, 'r');
 
     while(bytesRead = fs.readSync(fp, chunkBuffer, 0, chunkSize, offset)) {
-        offset += bytesRead;
-        console.log(chunkBuffer[0] + " " + String.fromCharCode(chunkBuffer[0]));
 
-        const rawData = chunkBuffer.slice(0, bytesRead);
-        const strData = rawData.toString();
+        let streamEnd = FindLastCharacter("\n", chunkBuffer, bytesRead);
 
-        let lineStart = 0;
-
-        // Split all the lines (account for when we don't read all the line)
-        while (true) {
-            let r = strData.indexOf("\n", lineStart);
-            if (r < 0) break;
-
-            const line = chunkBuffer.slice(lineStart, r - 1).toString();
-            totalLineCount += 1;
-
-            if (currentLine !== null) {
-                lines.push(`${currentLine}${line}`);
-                currentLine = null;
-            } else {
-                lines.push(line);
-            }
-
-            lineStart = r + 1;
+        if (streamEnd < 0) {
+            throw "The buffer is too small or the file isn't an CSV.";
+        } else {
+            offset += streamEnd + 1;
         }
 
-        // Trailling data, we will catch this next read
-        if (lineStart < bytesRead) {
-            const remainder = chunkBuffer.slice(lineStart, bytesRead).toString();
-            if (currentLine === null) {
-                currentLine = remainder;
-            } else {
-                currentLine += remainder;
-            }
+        // For the first pass, read the header & assign the filter
+        if (filter === null) {
+            filter = FilterHeaders(chunkBuffer.slice(0, bytesRead).toString().split("\n")[0]);
         }
 
-        if (lines.length > 250000) {
-            // For the first pass, read the header & assign the filter
-            if (filter === null) {
-                filter = FilterHeaders(lines[0]);
-                lines.shift();
-            }
-            SaveCSV(lines, filter, `./data/generated/CSV-${csvId++}.csv`);
-            lines = [];
-        }
+        SaveCSV(chunkBuffer, streamEnd, filter, `./data/generated/CSV-${csvId++}.csv`);
+
+        if (csvId > 4) break;
     }
 
-    if (currentLine !== null) {
-        totalLineCount += 1;
-        lines.push(currentLine);
-    }
-
-    if (lines.length > 0) {
-        SaveCSV(lines, filter, `./data/generated/CSV-${csvId++}.csv`);
-        lines = [];
-    }
-
-    if (totalLineCount != 31814267) {
-        console.error("Something is wrong, read " + totalLineCount + " lines, but should be 31814267");
-    }
+    console.log("Done.");
 }
 
 try {

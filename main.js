a2a66e7274997b915a6b019424bfd5709369794e
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-21 16:42:54 +0200

    worker communication

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
 import fs from 'fs';
-import { csv_to_table } from './csv.js'
+import pm2 from 'pm2';
+import { performance } from 'perf_hooks';
+import { csv_to_table } from './csv.js';
 
 const PATH = './data/StockEtablissement_utf8.csv';
+
+const WORK_IN_PROGRESS = {};
+const QUEUED_WORK = [];
+const FREE_INSTANCES = [];
+
+function UpdateWorkers(packet = null) {
+    if (packet !== null) {
+        // worker finished something
+        FREE_INSTANCES.push(packet.data.PID);
+        const delay = performance.now() - WORK_IN_PROGRESS[packet.data.FILE];
+        console.log("Took " + delay + " ms for " + packet.data.FILE);
+    }
+
+    if (QUEUED_WORK.length > 0 && FREE_INSTANCES.length > 0) {
+        let instanceReadyCount = Math.min(QUEUED_WORK.length, FREE_INSTANCES.length);
+
+        for (let i = 0; i < instanceReadyCount; ++i) {
+            const PID = FREE_INSTANCES[i];
+
+            // Remove it from the FREE list
+            FREE_INSTANCES.splice(i, 1);
+            
+            i -= 1;
+            instanceReadyCount -= 1;
+
+            const work = QUEUED_WORK.shift();
+
+            WORK_IN_PROGRESS[work] = performance.now();
+
+            // Send it the work it will handle
+            pm2.sendDataToProcessId(PID, {
+                type : 'process:msg',
+                data : {
+                    PID: PID,
+                    FILE: work
+                },
+                topic: "SIRENE-INVADER"
+            }
+            , (error, result) => {
+                if (error) console.error(error);
+            });
+        }
+    }
+}
+
+function StartCluster() {
+    pm2.start({
+        script  : 'worker.js',
+        name    : `worker`,
+        instances : "max",
+        exec_mode : "cluster"
+    }, (err, proc) => {
+        if (err) console.error(err);
+        else {
+            for (let i = 0; i < proc.length; ++i) {
+                FREE_INSTANCES.push(proc[i].pm_id);
+            }
+            UpdateWorkers();
+        }
+    });
+}
 
 function FilterHeaders(header) {
     let csv = csv_to_table(header);
@@ -31,13 +94,14 @@ function FilterHeaders(header) {
         if (idx < 0) throw "Header not found: " + headers[i];
         res.push(idx);
     }
-    console.log(res);
+    console.table(res);
     return res;
 }
 
-function SaveCSV(buffer, start, end, filter, savePath) {
+function SaveCSV(buffer, start, end, savePath) {
     fs.writeFileSync(savePath, buffer.slice(start, end));
-    console.log(savePath + " " + end);
+    QUEUED_WORK.push(savePath);
+    UpdateWorkers();
 }
 
 function FindFirstCharacter(character, buffer, bufferSize) {
@@ -87,7 +151,7 @@ function SplitCSV() {
             filter = FilterHeaders(chunkBuffer.slice(0, streamStart++).toString());
         }
 
-        SaveCSV(chunkBuffer, streamStart, streamEnd, filter, `./data/generated/CSV-${csvId++}.csv`);
+        SaveCSV(chunkBuffer, streamStart, streamEnd, `./data/generated/CSV-${csvId++}.csv`);
 
         if (csvId > 9) break; // FOR DEBUGGING
     }
@@ -95,12 +159,27 @@ function SplitCSV() {
     console.log("Done.");
 }
 
-try {
-    if (fs.existsSync(PATH)) {
-        SplitCSV();
-    } else {
-        console.error(`File doesn't exist: ${PATH}`);
+pm2.connect(function(err) {
+    if (err) {
+      console.error(err)
+      process.exit(2)
     }
-} catch(err) {
-    console.error(err)
-}
+
+    pm2.launchBus(function(err, pm2_bus) {
+        pm2_bus.on('process:msg', function(packet) {
+            UpdateWorkers(packet);
+        })
+    });
+
+    try {
+        if (fs.existsSync(PATH)) {
+            StartCluster();
+            SplitCSV();
+        } else {
+            console.error(`File doesn't exist: ${PATH}`);
+        }
+    } catch(err) {
+        console.error(err)
+    }
+  }
+);
--- worker.js
+++ worker.js
@@ -1,1 +1,23 @@
-console.log("Fuck you");
+setInterval(() => {
+    console.log("alive");
+}, 10000);
+
+var WORKING_FILE = "";
+var MY_PID = -1;
+
+function MarkAsCompleted() {
+    process.send({
+        type : 'process:msg',
+        data : {
+            FILE : WORKING_FILE,
+            PID: MY_PID
+        }
+    });
+}
+
+process.on('message', function(packet) {
+    WORKING_FILE = packet.data.FILE;
+    MY_PID = packet.data.PID;
+
+    MarkAsCompleted();
+});

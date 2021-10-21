f937d2ef8726354ee39dff83670f85100a1c5038
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-21 14:50:59 +0200

    generate proper header filter

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ import { csv_to_table } from './csv.js'
 const PATH = './data/StockEtablissement_utf8.csv';
 
 function FilterHeaders(header) {
-    console.log(csv_to_table(header));
-    return [0];
+    let csv = csv_to_table(header);
+    let res = [];
+
+    if (csv.length != 1) throw "Header doesn't exist.";
+
+    let table_header = csv[0];
+
+    const headers = [
+        "siren",
+        "nic",
+        "siret",
+        "dateCreationEtablissement",
+        "dateDernierTraitementEtablissement",
+        "typeVoieEtablissement",
+        "libelleVoieEtablissement",
+        "codePostalEtablissement",
+        "libelleCommuneEtablissement",
+        "codeCommuneEtablissement",
+        "dateDebut",
+        "etatAdministratifEtablissement"
+    ];
+
+    for (let i = 0; i < headers.length; ++i) {
+        let idx = table_header.indexOf(headers[i]);
+        if (idx < 0) throw "Header not found: " + headers[i];
+        res.push(idx);
+    }
+    console.log(res);
+    return res;
 }
 
 function SaveCSV(buffer, start, end, filter, savePath) {
